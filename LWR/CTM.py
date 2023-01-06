90847b2a3b68d153dce7ecf486a5b5bb7a3bd0ed
Junyi Ji <junyi.ji@vanderbilt.edu>
2023-01-06 16:35:22 -0600

    hello ctm!

--- LWR/CTM.py
+++ LWR/CTM.py
@@ -3,6 +3,14 @@
 """
 Created on Fri Jan  6 14:49:33 2023
 
+This python script is to reproduce the CTM simulation driven by the LWR model.
+
 @author: jjworklab
 """
 
+# Define the class for the CTM simulation with capacity drop
+class CTM_w_CD:
+# Attributes are listed below.
+
+    
+    
